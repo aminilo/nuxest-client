ff8e99db55c349bf9f8b7b9eb2aa4a75ddbabcb0
AmiNilo <aminilo.dev@gmail.com>
2025-07-05 17:23:53 +0330

    Refactor

--- services/authService.ts
+++ services/authService.ts
@@ -1,30 +1,23 @@
 import type { UpdateProfilePayload } from '~/types';
 import { api } from '~/utils/api';
 
-export async function getCsrfToken() {
+/*export async function getCsrfToken() {
   const res = await api().get('/csrf/csrf-token');
   return res.data.csrfToken;
-}
+}*/
 
 export async function login(email: string, password: string) {
   const csrfToken = await getCsrfToken();
-  return api().post('/auth/login', { email, password }, {
-    headers: { 'x-csrf-token': csrfToken }
-  });
+  return api().post('/auth/login', { email, password });
 }
 
 export async function register(email: string, password: string) {
   const csrfToken = await getCsrfToken();
-  return api().post('/auth/register', { email, password }, {
-    headers: { 'x-csrf-token': csrfToken }
-  });
+  return api().post('/auth/register', { email, password });
 }
 
 export async function logout() {
-  const csrfToken = await getCsrfToken();
-  return api().post('/auth/logout', {}, {
-    headers: { 'x-csrf-token': csrfToken }
-  });
+  return api().post('/auth/logout');
 }
 
 export async function getProfile() {
@@ -42,8 +35,7 @@ export async function updateProfile(payload: UpdateProfilePayload) {
 
   return api().patch('/auth/profile', data, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-      'x-csrf-token': csrfToken
+      'Content-Type': 'multipart/form-data'
     }
   });
 }
